ff73e700d0569224b8fd1b1cf28a96a2d631fc0c
Tomáš Bilina <bilint01@dixonscarphone.com>
2017-07-14 18:12:58 +0200

    get coordinates, render board, display predefined board

--- src/gameOfLife.js
+++ src/gameOfLife.js
@@ -2,35 +2,89 @@
 
 const playground = document.getElementById('playBoard'); // canvas element
 const boarddimension = playground.getContext('2d'); // dimension
-playground.style.border = '3px solid #2cc875';
+playground.style.border = '0.5px solid #555';
 
-// axis
-let x, y;
-x = y = 0;
+let a, b, value
 
-// cell
-const cell =() => {
-  boarddimension.fillRect(0, 0, 30, 30);
-  boarddimension.fillStyle = '#2cc875';
+// get coordinates of click over canvas
+playground.addEventListener("click", () => {
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = document.getElementById('playBoard');
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = event.pageY - totalOffsetY;
+
+    console.log(canvasX,canvasY)
+})
+
+// fill cell
+function fillCell(xaxis, yaxis, value) {
+    boarddimension.strokeRect(xaxis, yaxis, 30, 30)
+    boarddimension.strokeStyle = "#555"
+    if (value == 1) {
+        boarddimension.fillRect(xaxis, yaxis, 30, 30)
+        boarddimension.fillStyle = '#555'
+    } else {
+        boarddimension.fillRect(xaxis, yaxis, 30, 30)
+        boarddimension.fillStyle = '#fff'
+    }
+}
+
+// Clear fields
+function clearFields() {
+    fillCell(0, 0, 0)
+    for(a = 0; a <= 270; a+=30) {
+        for (b = 0; b <= 270; b += 30) {
+            fillCell(a, b, 0)
+        }
+    }
+}
+
+// fill fields
+function fillFields() {
+    clearFields()
+    //  render cells
+    const actualBoard = new Array(10)
+    for (a = 0; a <= 270; a += 30) {
+        actualBoard[a] = new Array(10)
+        for (b = 0; b <= 270; b += 30) {
+            if (a == 120 && b == 120) {
+                actualBoard[a][b] = 1
+            } else {
+                actualBoard[a][b] = 0
+            }
+        }
+    }
+
+    // render fields
+    for (a = 0; a <= 270; a += 30) {
+        for (b = 30; b <= 270; b += 30) {
+            fillCell(a, b, actualBoard[a][b])
+        }
+    }
+}
+
+// start game
+function gameStart() {
+    fillFields()
 }
 
 // controls
-document.getElementById('startButton').onclick = function(e) {
+document.getElementById('startButton').addEventListener("click", function(e){
     gameStart()
-}
-document.getElementById('overButton').onclick = function(e) {
-    gameOver()
-}
-
-
-let gameStart = function() {
-  console.log("successfully started")
-}
-
-let gameOver = function() {
-    console.log("It is sad but this game is over")
-}
-
+})
+document.getElementById('overButton').addEventListener("click", function(e){
+    clearFields()
+})
 
 // finally rendering
 
